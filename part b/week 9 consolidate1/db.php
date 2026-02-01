e28e4c84715160b3ef6f154555423711624c374c
hitarth-patel-hp <hitarth01patel@gmail.com>
2026-02-01 20:38:26 +0000

    Remove part b folder

--- part b/week 9 consolidate1/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "bookstore");
-session_start();
-
-if ($conn->connect_error) {
-    die("Database connection failed");
-}
-?>
--- part b/week 9 consolidate1/deactivate.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "db.php";
-
-$id = $_SESSION["user_id"];
-$conn->query("UPDATE users SET active=0 WHERE id=$id");
-
-session_destroy();
-echo "Account deactivated successfully.";
-?>
